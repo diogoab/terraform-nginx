96a46c3df8bb35a73264595df56d6a241a5b28a9
diogoab <diogo.alves.barbosa@gmail.com>
2021-06-18 18:47:47 -0300

    initial commit

---
+++ 03-security-groups.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "nginx-http-https" {
+  name        = "SG-WEB-http"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.VPC
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "WEB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+
+  ingress {
+    description      = "WEB-SSL"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow_HTTP_HTTPS"
+  }
+}
---
+++ 04-variables.tf
@@ -0,0 +1,31 @@
+variable "AMI" {
+    default = "ami-09e67e426f25ce0d7"
+}
+
+variable "NAME" {
+    default = "Nginx"
+}
+
+variable "VPC" {
+    default = ""
+}
+
+variable "SUBNET" {
+    default = ""
+}
+
+variable "AWS_ACESS_KEY_ID" {
+    default = "" 
+}
+
+variable "AWS_SECRET_ACESS_KEY" {
+   default =  ""
+} 
+
+variable "AWS_REGION" {
+    default = "us-east-1"
+}
+
+variable "KEY_NAME" {
+    default = ""
+}
